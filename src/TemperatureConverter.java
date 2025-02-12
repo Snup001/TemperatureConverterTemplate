a745a10c62fb85371199c765332a99d02e499d5a
YOUR_NAME <YOUR_EMAIL>
2025-02-12 14:29:23 -0500

    test

---
+++ src/TemperatureConverter.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class TemperatureConverter{
+    public double toCelsius(double fahrenheit) {
+            return 0.0;
+    }
+    public double toFahrenheit(double celsius) {
+        return 0.0;
+    }
+
+    public static void main(String [] args){
+        Scanner scnr = new Scanner(System.in);
+        TempratureConverter tc = new TemperatureConverter((celsius + 9/5) + 32);
+
+        newTemp = scnr.nextDouble();
+
+        
+        
+
+        System.out.println("Enter a temperature in Fahrenheit:" + newTemp );
+        System.out.println("")
+    
+        
+        git add TemperatureConverter.java
+        git commit -m "Implemented temperature converter"
+        git push origin main
+
+        git config --global user.email "Sn001@iup.edu";
+        git config --global user.name "Stefan Nikolic";
+    
+    
+    
+    }
+
+
+
+
+
+
+
+
+}
